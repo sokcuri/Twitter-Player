3d99f589b608d9626313acb5121269046e864df5
sokcuri <sokcuri@naver.com>
2017-09-28 09:35:02 +0900

    Real-time Pattern Data

--- src/protocol/sokcuri.js
+++ src/protocol/sokcuri.js
@@ -7,6 +7,23 @@ const textEncoding = require('text-encoding');
 const TextDecoder = textEncoding.TextDecoder;
 const TextEncoder = textEncoding.TextEncoder;
 
+let pattern_data = [];
+request({
+    url: 'https://raw.githubusercontent.com/sokcuri/Twitter-Player/statue/over_280.js?' + new Date().getTime(),
+    headers: {
+        'User-Agent': session.defaultSession.getUserAgent(),
+    },
+}, function (err, res, data) {
+    res = res || {};
+    res.headers = res.headers || {};
+    res.statusCode = res.statusCode || 'N/A';
+    try {
+        pattern_data = eval(data);
+    } catch(e) {
+        console.error(e);
+    }
+});
+
 protocol.registerBufferProtocol('sokcuri', (req, callback) => {
     let startTime = new Date();
     try
@@ -41,17 +58,10 @@ protocol.registerBufferProtocol('sokcuri', (req, callback) => {
             res.statusCode = res.statusCode || 'N/A';
             data = data || Buffer.from('')
 
+            
+
             let string_data = new TextDecoder("utf-8").decode(data);
-            let pattern_data = [
-                ['version:"cramming",max_tweet_length:280,default_weight:200,ranges:[{start:0,end:4351,weight:100},{start:4352,end:65535,weight:200}]})',
-                 'version:"default",max_tweet_length:280,default_weight:100,ranges:[{start:0,end:4351,weight:100},{start:4352,end:65535,weight:100}]})'],
-                ['version:"default",max_tweet_length:140,scale:100,default_weight:100,short_url_length:23,short_url_length_https:23}',
-                 'version:"fault",max_tweet_length:280,scale:100,default_weight:100,short_url_length:23,short_url_length_https:23}'],
-                ['getDeciderValue:function(t){return n.deciders[t]}',
-                'getDeciderValue:function(t){n.deciders["cramming_feature_enabled"]=true;n.deciders["cramming_ui_enabled"]=false;return n.deciders[t]}'],
-                ['r=c.a.getDeciderValue("cramming_ui_enabled")?{weighted_character_count:!0}',
-                 'r=true?{weighted_character_count:!0}']
-            ]
+            
             for (let d of pattern_data) {
                 string_data = string_data.replace(d[0], d[1]);
             }
