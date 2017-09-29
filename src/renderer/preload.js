daddb2852363d3b933e8609c60d4ba86e4cc064e
sokcuri <sokcuri@naver.com>
2017-09-29 14:11:31 +0900

    Fix to Tweet Embedding function

--- src/renderer/preload.js
+++ src/renderer/preload.js
@@ -7,6 +7,9 @@ ipcRenderer.on('uiShowMessage', (e, obj) => {
     $(document).trigger('uiShowMessage', {message: obj.message});
 });
 
-window.twttr = require('twitter-text');
+// twitter-text require시 window.twttr에 덮어씀
+// 공웹의 window.twttr는 트윗 담아가기로 쓰이기 때문에 undefined 처리해야 기능이 정상작동함
+window.twttxt = require('twitter-text');
+window.twttr = undefined;
 
 main.message('preload.js loaded', 'renderer');
